2055cefb139fa32118b2e989eee6326bb706c519
Alexandra Sanchez <alexlsanchez@gmail.com>
2020-10-29 22:15:01 +0000

    Corrected error

--- cw-minimum-maximum-values.py
+++ cw-minimum-maximum-values.py
@@ -1,29 +1,18 @@
-def minimum(arr):
-   """Returns minimum and maximum values of a given list."""
+def minimum_value(arr):
+   """Returns minimum value of a given list."""
    
-   minimum_num = []
-   
-   for dig in arr:
-       minimum_num.append(dig)
-    
-
-    return minimum_num.sort()
+   return min(arr)
         
-def maximum(arr):
+def maximum_value(arr):
+    """Returns maximum value of a given list"""
     
-    maximum_num = []
-    
-    for dig in arr:
-        maximum_num.append(dig)
-        maximum_num.sort()
+    return max(arr)
 
-    return maximum_num[-1]
-
-test1 = print(maximun([4,6,2,1,9,63,-134,566])) 
+test1 = print(maximum[4,6,2,1,9,63,-134,566])) 
 # returns 566
-test2 = print(minimun([-52, 56, 30, 29, -54, 0, -110]))
+test2 = print(maximum([-52, 56, 30, 29, -54, 0, -110]))
 # returns -110
-test3 = print(maximun([5]))
+test3 = print(maximum([5]))
 # returns 5
-test4 = print(minimun([42, 54, 65, 87, 0])) 
+test4 = print(maximum([42, 54, 65, 87, 0])) 
 # returns 0
